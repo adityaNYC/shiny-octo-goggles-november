022b4458c9fddb7498e5fbf430621ad5f5a7bf4d
Aditya <abalyan@una.edu>
2024-02-01 16:04:12 -0600

    updates November Lab

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5500;
+//set the view engine to ejs
+let path = require('path');
+app.set('view engine', 'ejs'); 
+app.set('views' , path.join(__dirname, 'views'));
+
+//use res.render to load up an ejs view file
+
+let myTypeServer = "Type 6 Enneagram)";
+
+app.get('/', function (req, res) {
+    res.render('index', {
+        myTypeClient: myTypeServer
+    });
+}); 
+
+//app.listen(3000)
+
+app.listen(port, () => {
+    console.log(`MIKE app listening on port ${port}`)
+})
